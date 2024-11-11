1dc10471b64796eeb00064bc7fc66b60eb95dded
zihaowu <zihao.wu@igg.com>
2024-11-11 14:38:52 +0800

    任务调度器中，一个agent只能被问询一次

--- captain/dispatcher.py
+++ captain/dispatcher.py
@@ -20,9 +20,7 @@ class DispatcherPrompt(Prompt):
                   'Task: What you need to do is to plan your workflow with your crews based on user query.\n'
                   '(User query might contains many steps, '
                   'think carefully about every step and plan your workflow with your crews)\n'
-                  "(User's query might need to call a crew member more than once, "
-                  'but think carefully about how many times a crew member needs to be called based on practical query, '
-                  'do not abuse or overuse him!)\n'
+                  "(A crew member can only be called once)\n"
                   "(Sometimes some of the crews are irrelevant to user's query. Make sure to call your crews properly.)\n"
                   'Output format: [{crew_member_1.name}, {crew_member_2.name}, ...] sequential and well-organized with no additional redundant information\n'
                   'Example output: [task_manager, bus_driver, song_writer]\n'
@@ -36,7 +34,8 @@ class DispatcherPrompt(Prompt):
         _fin_results = list()
         for _a_result in results:
             for crew in self.crews:
-                if crew.name == _a_result.strip():
+                if (crew not in _fin_results
+                        and crew.name == _a_result.strip()):
                     _fin_results.append(crew)
         return _fin_results
 
